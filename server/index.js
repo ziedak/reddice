0b3ef936cb10dcdfd51daaebee3a90db673ffd48
ziedak <ziedakremi@gmail.com>
2017-10-08 15:33:52 +0100

    Init commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import path from "path";
+
+let app = express();
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('run server port 3000')
+});
